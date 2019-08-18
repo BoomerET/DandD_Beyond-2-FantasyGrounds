4817873ed1d98ff6108f0a00f6d9f967585129b2
David Berkompas <david.berkompas@gmail.com>
2019-08-18 06:48:57 -0700

    Fix Fey Presence
    MIssing > symbole for group type in Fey Presence

--- js/app.js
+++ js/app.js
@@ -7497,7 +7497,7 @@ addWarlockFeyPresence=" \
 <description type=\"formattedtext\">\n \
 \t<p>Starting at 1st level, your patron bestows upon you the ability to project the beguiling and fearsome presence of the fey. As an action, you can cause each creature in a 10-foot cube originating from you to make a Wisdom saving throw against your warlock spell save DC. The creatures that fail their saving throws are all charmed or frightened by you (your choice) until the end of your next turn. Once you use this feature, you can't use it again until you finish a short or long rest.</p>\n \
 </description>\n \
-<group type=\"string\"Class Features</group>\n \
+<group type=\"string\">Class Features</group>\n \
 <level type=\"number\">0</level>\n \
 <locked type=\"number\">1</locked>\n \
 <name type=\"string\">Fey Presence</name>\n \
