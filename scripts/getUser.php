fc66483bb613594a7e4e7201330c64d4cfbc98a0
dberkompas <dberkompas@vindicia.com>
2020-02-06 13:23:33 -0800

    Syntax in getUser

--- scripts/getUser.php
+++ scripts/getUser.php
@@ -5,7 +5,7 @@
 
     $servername="localhost";
     $username = "root";
-    $password = "chip*two";
+    $password = "woogy";
     $dbname = "dnd2fg";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
